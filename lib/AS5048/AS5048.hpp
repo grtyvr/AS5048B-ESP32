8fefe68462c973d2003ed116933d1153a09e694a
guy <guy@grtyvr.com>
2020-11-05 13:18:19 -0800

    added circular mean and started adding comments

--- lib/AS5048/AS5048.hpp
+++ lib/AS5048/AS5048.hpp
@@ -11,11 +11,45 @@ class AS5048A{
     SPISettings settings;
 
     public:
+        /**
+         * @brief Constructor takes the chip select pin as an argument.
+         * 
+         *  @param {uint8_t} arg_cs Takes 
+         */
         AS5048A(uint8_t arg_cs);
-        void init();
+        /**
+         * @brief Initializer ets up the SPI bus for communications with the chip.  
+         * 
+         * @param clock default value of 1KHz
+         * @param bitOrder default value of MSBFIRST
+         * @param dataMode default value of SPI_MODE1 
+         */
+        void init(uint32_t clock = 1000000, uint8_t bitOrder = MSBFIRST, uint8_t dataMode = SPI_MODE1);
+        /**
+         * Close the SPI bus.  If you need to work with other devices on the same bus that do not have the same paramaters
+         * be sure to close the bus when you are done with a transaction.
+         */
         void close();
+        /**
+         * getMagnitude
+         * One of the diagnostic features of the AS5048 is that it can report the magnitude of the magnetic field
+         * @return {uint16_t} magnitude.
+         */
         uint16_t getMagnitude();
+        /**
+         * getAngle
+         * @return {uint16_t} angle as a value in the interval [0,2^14-1].  Rotation counter clockwise 
+         * from the current zero position
+         */
         uint16_t getAngle();
+        /**
+         * getExpSmoothAngle
+         * @param {float} smoothingFactor
+         * @return {uint16_t} angle as a value in the interval [0,2^14-1].  Rotation counter clockwise
+         * from the current zero position.  This value is caclulated from the last angle read and the 
+         * current angle read by the following formula
+         * new_angle = old_angle*(1 - smoothingFactor) + new_angle*smoothingFactor
+         */
         uint16_t getExpSmoothAngle(float smoothingFactor);
         uint16_t getAverageAngle(int numSamples);
         void printDiagnostics();
--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,29 @@ const uint8_t azPin = 5;
 
 AS5048A azSensor(azPin);
 
+
+
 void setup() {
   Serial.begin(9600);
   azSensor.init();
   azSensor.printDiagnostics();
+  delay(1000);
 }
 
 void loop() {
   Serial.print("Azimuth angle: ");
-  Serial.println(azSensor.getExpSmoothAngle(0.15));
+  Serial.println(azSensor.getAverageAngle(50));
+//  Serial.print("Azimuth angle: ");
+//  Serial.println(azSensor.getExpSmoothAngle(0.15));
 //  azSensor.printDiagnostics();
 //  Serial.print("         Gain: ");
 //  Serial.println(azSensor.getGain());
 //  azSensor.printDiagnostics();
-  if (azSensor.error()){
-    Serial.print("Error flag set: ");
-    uint8_t errors = azSensor.getErrors();
-    Serial.println(errors);
-  }
-  delay(10);
+//  if (azSensor.error()){
+//    Serial.print("Error flag set: ");
+//    uint8_t errors = azSensor.getErrors();
+//    Serial.println(errors);
+//  }
+
+  delay(100);
 }
